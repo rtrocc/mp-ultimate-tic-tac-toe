a0a89c5fbb1fafb3cb6f22b4604f6a590f82806d
Ralph Trocchia (Mac) <rtrocc@gmail.com>
2013-06-30 16:55:01 -0400

    adds daemon restart

--- server/daemon.js
+++ server/daemon.js
@@ -4,6 +4,15 @@ var daemon = require("daemonize2").setup({
     pidfile: "ultimatettt.pid"
 });
 
+var restart = false;
+
+daemon.on('stopped',function(){
+    if(restart){
+        restart = false;
+        daemon.start();
+    }
+});
+
 switch (process.argv[2]) {
 
     case "start":
@@ -14,6 +23,18 @@ switch (process.argv[2]) {
         daemon.stop();
         break;
 
+    case "restart":
+
+        if(!daemon.status()){
+            daemon.start();
+            break;
+        }else{
+            restart = true;
+            daemon.stop()
+        }
+               
+        break;
+    
     case "status":        
         console.log(daemon.status()?'Running as '+daemon.status():'Not Running'); 
         break;
