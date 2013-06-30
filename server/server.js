da125e9fd158b7a68bf079cc03a769e0eb6a6e73
Ralph Trocchia (Mac) <rtrocc@gmail.com>
2013-06-30 18:49:43 -0400

    playerlist in client and full room block

--- server/server.js
+++ server/server.js
@@ -2,10 +2,29 @@ var io = require('socket.io').listen(8001,{
 	'log level': 2
 });
 
+var rooms = {};
 
 io.sockets.on('connection', function (socket) {
 
+	socket.meta = {};
+
 	socket.on('subscribe', function (data) {
+
+
+		if(rooms[data.room]){
+			if(rooms[data.room].playerList.length >= 2){
+				socket.emit('error', {'errorType':'fullroom'});
+				return;
+			}
+			rooms[data.room].playerList.push(data.name);
+		}else{
+			rooms[data.room] = {
+				playerList : []
+			};
+			rooms[data.room].playerList.push(data.name);
+		}
+
+		console.log(rooms[data.room].playerList);
 
 		socket.join(data.room);
 
@@ -13,7 +32,7 @@ io.sockets.on('connection', function (socket) {
 		socket.meta.room = data.room;
 		socket.meta.name = data.name
 
-		io.sockets.in(socket.meta.room).emit('userjoined',{'name':data.name});
+		io.sockets.in(socket.meta.room).emit('userjoined',{'name':data.name,'playerList':rooms[data.room].playerList});
 	});
 
 	socket.on('onmove', function (data) {
@@ -22,7 +41,21 @@ io.sockets.on('connection', function (socket) {
 
 	socket.on('disconnect', function() {
 		console.log(socket.meta.name+' disconnected!');
-		io.sockets.in(socket.meta.room).emit('userdisconnect',{'name':socket.meta.name});
+		if(rooms[socket.meta.room]){
+			for(i in rooms[socket.meta.room].playerList){
+				if(rooms[socket.meta.room].playerList[i] == socket.meta.name){
+					rooms[socket.meta.room].playerList.splice(i,1);
+				}
+			}	
+		}
+		io.sockets.in(socket.meta.room).emit('userdisconnect',{
+			'name': socket.meta.name,
+			'playerList': (rooms[socket.meta.room]?rooms[socket.meta.room].playerList:false)
+		});
+	})
+
+	socket.on('connect', function() {
+		
 	})
 
 	//console.log(io.sockets.sockets);
